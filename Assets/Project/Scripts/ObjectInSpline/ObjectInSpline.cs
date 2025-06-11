b481f3570067de86ee076e876a32caa6db941782
Matteo Golfetto <Matteogol16@gmail.com>
2025-06-11 14:26:13 +0200

    smoothing movement of the object

--- Assets/Project/Scripts/ObjectInSpline/ObjectInSpline.cs
+++ Assets/Project/Scripts/ObjectInSpline/ObjectInSpline.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Splines;
@@ -8,34 +10,34 @@ public class ObjectInSpline : MonoBehaviour
 
     bool isSelected = false;
 
+    float currentPosition = 0f;
+    float targetPosition = 0f;
+
     private void Update()
     {
         SetSelected();
-        MoveWithMouse();
+        SetTargetPosition();
+        MoveObject();
     }
 
-    private void MoveWithMouse()
+    private void MoveObject()
+    {
+        //if (!isSelected) return;
+
+        currentPosition = Mathf.Lerp(currentPosition, targetPosition, Time.deltaTime * 5f);
+
+        transform.position = SplineUtility.EvaluatePosition(splineContainer[0], currentPosition);
+    }
+
+    private void SetTargetPosition()
     {
         if(!isSelected) return;
 
         if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
-            {
-                float3 hitPoint = hit.point;
 
-                SplineUtility.GetNearestPoint(splineContainer[0], hitPoint, out float3 distance, out float t);
-
-                Vector3 positionOnSpline = SplineUtility.EvaluatePosition(splineContainer[0], t);
-
-                Vector3 worldPositionOnSpline = transform.TransformPoint(positionOnSpline);
-
-                Debug.DrawLine(hitPoint, positionOnSpline, Color.red);
-
-                transform.position = positionOnSpline;
-            }
+            SplineUtility.GetNearestPoint(splineContainer[0], ray, out float3 distance,out targetPosition);
         }
     }
 
