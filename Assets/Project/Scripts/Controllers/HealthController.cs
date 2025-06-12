e78eba3075ebedd38b7fe50b8bd2912fc59929ac
Matteo Golfetto <Matteogol16@gmail.com>
2025-06-12 15:26:50 +0200

    Added HealthController

---
+++ Assets/Project/Scripts/Controllers/HealthController.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+namespace utilities.Controllers
+{
+    public class HealthController : MonoBehaviour
+    {
+        public delegate void OnDeath();
+        public delegate void OnDamage(float MaxHp, float currentHp);
+
+        public OnDeath onDeath;
+        public OnDamage onDamage;
+
+        [Header("HP options")]
+        [SerializeField] int maxHitPoints;
+        [SerializeField] int currentHp;
+        [SerializeField] bool invincible;
+
+        [Header("Death Animation")]
+        [SerializeField] GameObject deathEffect;
+
+        /// <summary>
+        /// sets the invincibility state of the health controller.
+        /// </summary>
+        public void SetInvicible(bool invincible) { this.invincible = invincible; }
+        public int CurrentHp { get => currentHp; }
+        public int MaxHitPoints { get => currentHp; }
+
+
+        private void Start()
+        {
+            currentHp = maxHitPoints;
+            onDamage?.Invoke(maxHitPoints, currentHp);
+        }
+
+        /// <summary>
+        /// Applies damage to the health controller.
+        /// it also fires the onDamage event and if the health reaches 0 it fires the onDeath event.
+        /// </summary>
+        /// <param name="damage"> the damage to apply</param>
+        public void DoDamage(int damage)
+        {
+            if (invincible)
+                return;
+
+            currentHp -= damage;
+            onDamage?.Invoke(maxHitPoints, currentHp);
+
+            if (currentHp <= 0)
+            {
+                if (deathEffect != null)
+                {
+                    GameObject clone = Instantiate(deathEffect, transform.position, Quaternion.identity);
+                    Destroy(clone, clone.GetComponent<ParticleSystem>().main.duration);
+                }
+
+                onDeath?.Invoke();
+            }
+        }
+
+        /// <summary>
+        /// Resets the health to the maximum hit points.
+        /// </summary>
+        public void ResetHealth()
+        {
+            currentHp = maxHitPoints;
+            onDamage?.Invoke(maxHitPoints, currentHp);
+        }
+
+        /// <summary>
+        /// Restores health by a specified amount, without exceeding the maximum hit points.
+        /// </summary>
+        /// <param name="amount">The amount of health to restore.</param>
+        public void RestoreHealth(int amount)
+        {
+            currentHp += amount;
+            currentHp = Mathf.Min(currentHp, maxHitPoints);
+            onDamage?.Invoke(maxHitPoints, currentHp);
+        }
+    }
+}
