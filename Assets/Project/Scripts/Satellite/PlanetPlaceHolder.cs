de5a74979e6ab5d34fc8b0cc753690b457c56e8c
Matteo Golfetto <Matteogol16@gmail.com>
2025-06-24 18:35:45 +0200

    fix del fix

--- Assets/Project/Scripts/Satellite/PlanetPlaceHolder.cs
+++ Assets/Project/Scripts/Satellite/PlanetPlaceHolder.cs
@@ -6,8 +6,5 @@ public class PlanetPlaceHolder : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(transform.position, GetComponent<GravityPullToggle>().PullRadius);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawSphere(transform.position, 0.4f);
     }
 }
--- Assets/Project/Scripts/Satellite/SatellitePlaceholder.cs
+++ Assets/Project/Scripts/Satellite/SatellitePlaceholder.cs
@@ -6,5 +6,8 @@ public class SatellitePlaceholder : MonoBehaviour
     private void OnDrawGizmosSelected()
     {
         Gizmos.DrawWireSphere(transform.position, GetComponent<Satellite>().Radius);
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.4f);
     }
 }
