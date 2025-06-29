045e55747141f763b0bc57089c9f95c9d1828bc5
Giovanni Faldani <faldanig@yahoo.it>
2025-06-29 21:44:11 +0200

    Add parallax background

---
+++ Assets/Project/Scripts/Parallax/ParallaxLayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ParallaxLayer : MonoBehaviour
+{
+    private Vector3 startingPos;
+    public float amountOfParallax;
+
+    private void Start()
+    {
+        startingPos = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 position = Camera.main.transform.position;
+        float distanceX = position.x * amountOfParallax;
+        float distanceZ = position.z * amountOfParallax;
+
+        transform.position = new Vector3(startingPos.x + distanceX, transform.position.y, startingPos.z + distanceZ);
+    }
+}
